c3642f208eb2a5f0b1f0bbe0404733cbaea67978
Pulkit Gupta <pulgupta@gmail.com>
2019-04-03 01:20:07 +0530

    initial commit

---
+++ src/main/java/com/example/docker/pipeline/integrationtests/IntegrationTestsApplication.java
@@ -0,0 +1,13 @@
+package com.example.docker.pipeline.integrationtests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegrationTestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegrationTestsApplication.class, args);
+	}
+
+}
